145e4bc86893d6813a984af9dca3acd6d817c5c7
yuzosergio <yuzosergio@hotmail.com>
2019-08-30 10:34:35 -0300

    Banco de dados

--- src/main/java/com/yuzosergio/Cursomc/domain/Categoria.java
+++ src/main/java/com/yuzosergio/Cursomc/domain/Categoria.java
@@ -2,11 +2,19 @@ package com.yuzosergio.Cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 	
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	private String nome;
 	
