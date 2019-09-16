6ac31c328de11b1755f72ef05b75b03f16ebce93
yuzosergio <yuzosergio@hotmail.com>
2019-09-16 16:16:33 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/yuzosergio/Cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/yuzosergio/Cursomc/resources/exception/ResourceExceptionHandler.java
@@ -2,11 +2,14 @@ package com.yuzosergio.Cursomc.resources.exception;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.hibernate.exception.DataException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.yuzosergio.Cursomc.services.exceptions.DataIntegrityException;
 import com.yuzosergio.Cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -16,5 +19,11 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e,HttpServletRequest request){
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(),e.getMessage(),System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e,HttpServletRequest request){
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),System.currentTimeMillis());			
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 }
--- src/main/java/com/yuzosergio/Cursomc/services/CategoriaService.java
+++ src/main/java/com/yuzosergio/Cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.yuzosergio.Cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.yuzosergio.Cursomc.domain.Categoria;
 import com.yuzosergio.Cursomc.repositories.CategoriaRepository;
+import com.yuzosergio.Cursomc.services.exceptions.DataIntegrityException;
 import com.yuzosergio.Cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,5 +32,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id); 
+		}
+		catch(DataIntegrityViolationException e){
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produto");
+			
+		}
+		
 	}
 	}
