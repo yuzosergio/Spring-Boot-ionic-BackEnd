2dc9b186129eb6bd9bceb33093bd9edd436de4b2
yuzosergio <yuzosergio@hotmail.com>
2019-11-05 15:52:04 -0300

    Listando categorias com DTO

--- src/main/java/com/yuzosergio/Cursomc/services/CategoriaService.java
+++ src/main/java/com/yuzosergio/Cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.yuzosergio.Cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,5 +44,8 @@ public class CategoriaService {
 			
 		}
 		
+	}
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 	}
